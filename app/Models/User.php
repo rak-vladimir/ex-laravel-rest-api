c1644f1e55d08bd4d3b68cfa3eb738df09137008
Rak Vladimir <made.in.cccp85@gmail.com>
2026-03-09 03:38:22 +0500

    feat: setup proper API routes with Sanctum example and rate limiting

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * @property int                             $id
+ * @property string                          $name
+ * @property string                          $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string                          $password
+ * @property string|null                     $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ */
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
@@ -42,7 +52,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            'password'          => 'hashed',
         ];
     }
 }
