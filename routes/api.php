5400e80bb71441d29ca7b1ee25dd89e7fa724389
Rak Vladimir <made.in.cccp85@gmail.com>
2026-03-09 11:29:46 +0500

    feat(auth): add controller, api endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login',    [AuthController::class, 'login']);
 
-Route::get('/test', function () {
-    return response()->json(['message' => 'API is working!']);
+Route::middleware('auth:sanctum')->group(function (Request $request) {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user',    [AuthController::class, 'user']);
 });
